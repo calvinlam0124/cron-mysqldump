01a254f7df55fd249a2509289feedcbeaa3b883b
Calvin Lam <calvinlam117@gmail.com>
2018-01-30 16:06:44 +0800

    Change to default mysql database and a reachable host

--- mysqldump.conf.d/conf.d/database_example.mysqldump.sh
+++ mysqldump.conf.d/conf.d/database_example.mysqldump.sh
@@ -4,9 +4,9 @@ MYSQLDUMP=/usr/bin/mysqldump
 
 MYSQL_USER=root
 MYSQL_PASSWORD=111111
-MYSQL_HOST=127.0.0.1
+MYSQL_HOST=192.168.1.96
 MYSQL_PORT=3306
-MYSQL_DATABASE=test
+MYSQL_DATABASE=mysql
 
 DATE_FORMAT=`date "+%Y%m%d.%Hh%Mm%Ss"`
 OUTPUT_DIR=/mnt/mysqldump.conf.d/data/
